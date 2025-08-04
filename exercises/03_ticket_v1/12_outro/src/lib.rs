c6213b961085271dd9e05823fc95f7fd5f7d2a09
fgarnadi <fajar.garnadi@gmail.com>
2025-08-04 17:28:27 +0700

    finish exercise 3

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,71 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        if product_name.is_empty() || product_name.len() > 300 {
+            panic!("product name constraint not met.")
+        }
+
+        if quantity <= 0 {
+            panic!("quantity constraint not met.")
+        }
+
+        if unit_price <= 0 {
+            panic!("unit price constraint not met")
+        }
+
+        Self {
+            product_name: product_name,
+            quantity: quantity,
+            unit_price: unit_price,
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        if product_name.is_empty() || product_name.len() > 300 {
+            panic!("product name constraint not met.")
+        }
+
+        self.product_name = product_name;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        if quantity <= 0 {
+            panic!("quantity constraint not met.")
+        }
+
+        self.quantity = quantity;
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        if unit_price <= 0 {
+            panic!("unit price constraint not met")
+        }
+
+        self.unit_price = unit_price;
+    }
+}
